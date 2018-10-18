2e8b8fd0bf84c3a6ed7b5646f7b124bc62a15856
ashutosh28283 <ashutosh28283@gmail.com>
2018-10-18 19:00:03 +0530

    Create ashu10.java

---
+++ ashu10.java
@@ -0,0 +1,11 @@
+import.java.util.*;
+class Solution
+{
+  public static void main(String[] args)
+  {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int k=(n*(n+1))/2;
+      System.out.println(k);
+      }
+      }
