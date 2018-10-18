24a701928b9e0bddda312bff389aaaa290afafe4
ashutosh28283 <ashutosh28283@gmail.com>
2018-10-18 14:30:50 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Solution
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+{
+System.out.println("Positive");
+}
+elif(a<0)
+{
+System.out.println("Negative");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
