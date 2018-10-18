51dcc1792673fc4af3eb2ee75bd395b8bd2d65b1
ashutosh28283 <ashutosh28283@gmail.com>
2018-10-18 14:40:14 +0530

    Create ashu.java

---
+++ ashu.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution
+{
+public static void main (String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%2==0)
+{
+System.out.println("Even");
+}
+else if(a%2==1)
+{
+System.out.println("Odd");
+}
+else
+{
+System.out.println("Invalid");
+}
+}
+}
