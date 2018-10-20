01d03c0635fa5f8bb1ba78bbd712c55886b435a1
ashutosh28283 <ashutosh28283@gmail.com>
2018-10-20 00:06:17 -0700

    Create ashu28.java

---
+++ ashu28.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution
+{
+public static void main(String[] args)
+{
+  Scanner sc=new Scanner(System.in);
+int temp;
+  int n=sc.nextInt();
+  int[] a=new int[n];
+  for(int i=0;i<n;i++)
+  {
+  a[i]=sc.nextInt();
+  }
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n-i-1;j++)
+    {
+      if(a[j]>a[j+1])
+      {
+      temp=a[j];
+      a[j]=a[j+1];
+      a[j+1]=temp;
+      }
+    }
+    }
+    System.out.println(a[n-1]);
+    }
+    }
